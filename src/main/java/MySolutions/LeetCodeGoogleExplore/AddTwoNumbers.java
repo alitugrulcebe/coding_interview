448488090362433c97d464c583ddd53fc51f2459
Ali Tugrul Cebe <>
2019-06-29 18:09:08 -0500

    Studying...

--- src/main/java/MySolutions/LeetCodeGoogleExplore/AddTwoNumbers.java
+++ src/main/java/MySolutions/LeetCodeGoogleExplore/AddTwoNumbers.java
@@ -3,7 +3,20 @@ package MySolutions.LeetCodeGoogleExplore;
 import MySolutions.LinkList.ListNode;
 
 public class AddTwoNumbers {
-
+    /**
+     * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+     *
+     * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+     *
+     * Example:
+     *
+     * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+     * Output: 7 -> 0 -> 8
+     * Explanation: 342 + 465 = 807.
+     * @param l1
+     * @param l2
+     * @return
+     */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(-1);
         ListNode cur = dummy;
